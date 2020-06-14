5d00dab7253365710f4f320c1e9059879ee95a98
Anton Lenok <dzx912@gmail.com>
2020-06-14 16:04:02 +0300

    Add documentation for drawing a chart

--- moex_chart.py
+++ moex_chart.py
@@ -12,6 +12,22 @@ class MoexChart:
         pass
 
     def draw(self, data_frame, security_name, date):
+        """
+        Create image with chart of changing security price
+        Image will put to path 'chart/{security_name}'
+
+        :param data_frame: data for visualization.
+        Format:
+        |----------|----------|
+        |---date---|---price--|
+        |----------|----------|
+        |2020-04-01|1000.00   |
+        |2020-04-02|1001.00   |
+        |----------|----------|
+        Where column 'date' is index
+        :param security_name: name of security. User for file name and top of chart.
+        :param date: date from start show. Use like filter.
+        """
         day_price = data_frame[date:].price
         week_price = data_frame.price.rolling(5).mean()[date:]
         month_price = data_frame.price.rolling(20).mean()[date:]
@@ -25,9 +41,9 @@ class MoexChart:
         ax.set_title(security_name + ' график цен')
 
         plt.plot(date_x, day_price, 'red', label='Цена закрытия')
-        plt.plot(date_x, week_price, 'y', label='Недельная скользящая средняя')
-        plt.plot(date_x, month_price, 'b', label='Месячная скользящая средняя')
-        plt.plot(date_x, year_price, 'g', label='Годовая скользящая средняя')
+        plt.plot(date_x, week_price, 'yellow', label='Недельная скользящая средняя')
+        plt.plot(date_x, month_price, 'blue', label='Месячная скользящая средняя')
+        plt.plot(date_x, year_price, 'green', label='Годовая скользящая средняя')
 
         ax.set_ylabel('Рубли')
         ax.set_xlabel('Дата')
